9311ae862fca250da8f643161b6ced95c72ff5e0
AnilPenur <anilpenur@gmail.com>
2023-06-17 15:00:25 +0530

    minor changes made in function evaluate_models

--- src/utils.py
+++ src/utils.py
@@ -25,7 +25,8 @@ def  evaluate_models(X_train,y_train,X_test,y_test,models:dict):
     try:
         report = dict()
         for model_name,model in models.items():
-            model.fit(X_train, y_train)
+            logging.info(model)
+            model.fit(X = X_train, y = y_train)
             preds = model.predict(X_test)
             r2 = r2_score(y_test, preds)
             report[model_name] = r2
