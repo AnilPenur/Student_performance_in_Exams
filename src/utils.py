212e6cdc8d548f5fba30d03d6bfacc2a7e1b6b2d
AnilPenur <anilpenur@gmail.com>
2023-06-21 08:37:16 +0530

    CustomData() class added to utility which will create dataframe when datapoint values are given to it as parameters

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import dill
 import pickle
 import os
 import sys
+import pandas as pd
 
 from src.logger import logging
 from src.exception import CustomException_ANIL
@@ -34,3 +35,56 @@ def  evaluate_models(X_train,y_train,X_test,y_test,models:dict):
         return report
     except Exception as e:
         raise CustomException_ANIL(e, sys)
+        
+
+def load_object(file_path:str):
+    '''It will help in loading object(pickel) file using dill module'''
+    try:
+        with open(file_path,mode="rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CUstomException_ANIL(e,sys)
+
+
+class CustomData:
+    '''
+    calss CustomData()::
+    In constructor it will take all features of a datapoint as patrameters.
+    The method of the class `` will convert this datapoint into a dataframe having 
+    columns as features and row having values.
+    '''
+    def __init__(self, 
+                gender:str, 
+                race_ethnicity:str, 
+                parental_level_of_education:str,
+                lunch:str,
+                test_preparation_course:str,
+                reading_score:int,
+                writing_score:int):
+
+                self.gender = gender
+                self.race_ethnicity = race_ethnicity
+                self.parental_level_of_education = parental_level_of_education
+                self.lunch = lunch
+                self.test_preparation_course = test_preparation_course
+                self.reading_score = reading_score
+                self. writing_score = writing_score
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict:dict ={
+                'gender':[self.gender], 
+                'race_ethnicity':[self.race_ethnicity], 
+                'parental_level_of_education':[self.parental_level_of_education],
+                'lunch':[self.lunch],
+                'test_preparation_course':[self.test_preparation_course],
+                'reading_score':[self.reading_score],
+                'writing_score':[self. writing_score]
+             }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException_ANIL(e,sys)
+
+
